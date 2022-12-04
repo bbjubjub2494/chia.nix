ec4f296efa4d42c1d6a73a6ba085b059cf1dcbb6
Louis Bettens <louis@bettens.info>
2022-12-04 18:36:19 +0100

    document causes of custom dependencies

--- packages/default.nix
+++ packages/default.nix
@@ -2,13 +2,18 @@
   pkgs' = pkgs.extend (final: prev: {
     python3Packages = prev.python3Packages.override {
       overrides = final': prev': {
+        # https://nixpk.gs/pr-tracker.html?pr=200769
         blspy = final'.callPackage ./blspy {};
         chia-rs = final'.callPackage ./chia-rs {};
         chiapos = final'.callPackage ./chiapos {};
         chiavdf = final'.callPackage ./chiavdf {};
         clvm-tools = final'.callPackage ./clvm-tools {};
         clvm-tools-rs = final'.callPackage ./clvm-tools-rs {};
+
+        # https://nixpk.gs/pr-tracker.html?pr=202239
         typing-extensions = final'.callPackage ./typing-extensions {};
+        
+        # https://nixpk.gs/pr-tracker.html?pr=202242
         cryptography = final'.callPackage ./cryptography {
           inherit (final.darwin) libiconv;
           inherit (final.darwin.apple_sdk.frameworks) Security;
