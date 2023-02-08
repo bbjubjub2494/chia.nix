e23146de74bb69c8b2051c6f40e3d5f3c2a6975a
Louis Bettens <louis@bettens.info>
2023-02-08 09:01:03 +0100

    chia: 1.7.0rc6 -> 1.7.0rc7

--- packages/chia-rc/default.nix
+++ packages/chia-rc/default.nix
@@ -8,14 +8,14 @@
 let
   chia = python3Packages.buildPythonApplication rec {
     pname = "chia";
-    version = "1.7.0-rc6";
+    version = "1.7.0-rc7";
 
     src = fetchFromGitHub {
       owner = "Chia-Network";
       repo = "chia-blockchain";
       rev = version;
       fetchSubmodules = true;
-      hash = "sha256-kdxp70AM+LtilqwLOYpVb3ybFrPEDwsxH8RRuhHCqiI=";
+      hash = "sha256-3qHSxAXgpBrP2v86O06UrHuInQ08TO5G8l4u2zJGVZg=";
     };
 
     postPatch = ''
