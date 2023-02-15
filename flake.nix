f6d72ffef72d0b1933cbdb423b05585def631d03
Louis Bettens <louis@bettens.info>
2023-02-15 18:25:40 +0100

    flake: refactor using flake-parts

--- flake.nix
+++ flake.nix
@@ -3,26 +3,25 @@
   description = "A reproducible Nix package set for Chia";
 
   # Inputs are how Nix can use code from outside the flake during evaluation.
-  inputs.fup.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
   inputs.cat-admin-tool.url = "github:Chia-Network/CAT-admin-tool";
   inputs.cat-admin-tool.flake = false;
 
   # Outputs are the public-facing interface to the flake.
-  outputs = { self, fup, nixpkgs, ... } @ inputs:
-    fup.lib.mkFlake {
-      inherit self inputs;
-
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-
-      channels.nixpkgs.overlaysBuilder = _: [
-        (import ./overlay.nix inputs)
+  outputs = { self, flake-parts, nixpkgs, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
       ];
 
-      outputsBuilder = channels: {
-        formatter = channels.nixpkgs.nixpkgs-fmt;
-        packages = channels.nixpkgs.chiaNix;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { final, prev, ... }: {
+        overlayAttrs = import ./overlay.nix inputs final prev;
+
+        formatter = final.nixpkgs-fmt;
+        packages = final.chiaNix;
       };
     };
 }
