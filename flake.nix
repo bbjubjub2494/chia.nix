377eb711c03b2b58ebb9669681b724c729361ce9
Louis Bettens <louis@bettens.info>
2023-02-17 09:48:42 +0100

    flake: add binary cache

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
   inputs.cat-admin-tool.url = "github:Chia-Network/CAT-admin-tool";
   inputs.cat-admin-tool.flake = false;
 
+  nixConfig.extra-substituters = "https://chia-nix.cachix.org";
+  nixConfig.extra-trusted-public-keys = "chia-nix.cachix.org-1:Zf5INmp8F07mHCCslASHUx7ue+GbwxxV5Uw7e5DdUDI=";
+
+
   # Outputs are the public-facing interface to the flake.
   outputs = { self, flake-parts, nixpkgs, ... } @ inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
