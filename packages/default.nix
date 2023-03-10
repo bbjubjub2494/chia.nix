c3e52a1308a74a78dbce6f7b23ca92a0dab84cb4
Julie B. <no-reply@bbjubjub.fr>
2023-03-10 13:10:09 +0100

    python3Packages.aiofiles: 23.1.0

--- packages/default.nix
+++ packages/default.nix
@@ -18,6 +18,7 @@ pkgs.extend (final: prev:
     overrides = final': prev':
       replaceOlderAttr prev'
         {
+          aiofiles = final'.callPackage python/aiofiles { };
           aiohttp = final'.callPackage python/aiohttp { };
           chia-rs_0_2_2 = final'.callPackage python/chia-rs_0_2_2 { };
           chia-rs_0_2_0 = final'.callPackage python/chia-rs_0_2_0 { };
