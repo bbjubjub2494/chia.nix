59a78690635fbc728894e634cbea7ec1312fafb2
Julie B. <no-reply@bbjubjub.fr>
2023-12-02 14:52:42 +0100

    nix: automatically enumerate packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,44 +1,21 @@
 { inputs, pkgs }:
 let
-  replaceOlder = refPkg: altPkg:
-    if pkgs.lib.versionOlder (pkgs.lib.getVersion refPkg) (pkgs.lib.getVersion altPkg)
-    then altPkg
-    else pkgs.lib.warn "chia.nix: not replacing ${pkgs.lib.getName refPkg} ${pkgs.lib.getVersion refPkg} with ${pkgs.lib.getName altPkg} ${pkgs.lib.getVersion altPkg} because it's not older" refPkg;
-  replaceOlderAttr = refs: alts:
-    pkgs.lib.mapAttrs
-      (name: pkg:
-        if pkgs.lib.hasAttr name refs
-        then replaceOlder (pkgs.lib.getAttr name refs) pkg
-        else pkg)
-      alts;
+  inherit (pkgs) lib;
+  collect = dir:
+    if !builtins.pathExists dir
+    then { }
+    else
+      lib.concatMapAttrs
+        (name: type:
+          if type == "directory" && builtins.pathExists (dir + "/${name}/default.nix")
+          then { ${name} = dir + "/${name}"; }
+          else { })
+        (builtins.readDir dir);
 in
 pkgs.extend (final: prev:
 {
   python3Packages = prev.python3Packages.override {
     overrides = final': prev':
-      {
-        based58 = final'.callPackage python/based58 { };
-        blspy = final'.callPackage python/blspy { };
-        chia-rs = final'.callPackage python/chia-rs { };
-        chiabip158 = final'.callPackage python/chiabip158 { };
-        chiavdf = final'.callPackage python/chiavdf { };
-        chiapos = final'.callPackage python/chiapos { };
-        clvm = final'.callPackage python/clvm { };
-        clvm-rs = final'.callPackage python/clvm-rs { };
-        clvm-tools = final'.callPackage python/clvm-tools { };
-        clvm-tools-rs = final'.callPackage python/clvm-tools-rs { };
-      };
+      lib.mapAttrs (_: path: final'.callPackage path { }) (collect ./python);
   };
-  chia-dev-tools = final.callPackage ./chia-dev-tools { };
-
-  # https://nixpk.gs/pr-tracker.html?pr=201542
-  bladebit = final.callPackage ./bladebit { };
-
-  # not suitable for Nixpkgs
-  cat-admin-tool = final.callPackage ./cat-admin-tool { };
-  chia = final.callPackage ./chia { };
-  chia-beta = final.callPackage ./chia-beta { };
-  chia-rc = final.callPackage ./chia-rc { };
-  chia-plotter = final.callPackage ./chia-plotter { };
-  dexie-rewards = final.callPackage ./dexie-rewards { };
-})
+} // lib.mapAttrs (_: path: final.callPackage path { }) (collect ./.))
